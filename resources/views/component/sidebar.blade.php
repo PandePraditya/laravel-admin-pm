688fe643d1a16407614f72a31e93c0433da8b39a
PandePraditya <pandepraditya@gmail.com>
2024-06-26 13:22:00 +0800

    fix minor issue

--- resources/views/component/sidebar.blade.php
+++ resources/views/component/sidebar.blade.php
@@ -17,9 +17,9 @@
 </nav> --}}
 
 {{-- Off canvas navbar --}}
-<nav class="navbar bg-body-tertiary fixed-top ps-4">
+{{-- <nav class="navbar bg-body-tertiary fixed-top ps-4">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ route('category') }}">Laravel</a>
+        <a class="navbar-brand" href="{{ route('category.index') }}">Laravel</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
             aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -32,7 +32,7 @@
             <div class="offcanvas-body">
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('category') }}">Home</a>
+                        <a class="nav-link active" aria-current="page" href="{{ route('category.index') }}">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('login') }}">Login</a>
@@ -58,8 +58,8 @@
             </div>
         </div>
     </div>
-</nav>
-{{-- 
+</nav> --}}
+
 <aside class="main-sidebar sidebar-dark-primary elevation-4 z-1">
     <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary vh-100" style="width: 280px;">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
@@ -130,4 +130,4 @@
             </ul>
         </div>
     </div>
-</aside> --}}
+</aside>
